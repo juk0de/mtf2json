eb8b775ea7208f3cb8fbfe4ccf16bfd67b243c8e
juk0de <git@kadidlo.de>
2024-07-07 20:34:26 +0200

    cli: '--mtf-file' and '--json-file' now support muliple arguments

--- mtf2json/cli.py
+++ mtf2json/cli.py
@@ -16,14 +16,16 @@ def create_parser() -> argparse.ArgumentParser:
     # options
     parser.add_argument('--mtf-file', '-m',
                         type=str,
-                        help="The MTF file to convert.",
+                        nargs='+',
+                        help="The MTF file(s) to convert.",
                         metavar="MTF_FILE")
     parser.add_argument('--convert', '-c',
                         action='store_true',
                         help="Convert the MTF file to a JSON file (use same filename with suffix '.json').")
     parser.add_argument('--json-file', '-j',
                         type=str,
-                        help="The destination file for JSON conversion (instead of default filename).",
+                        nargs='+',
+                        help="The destination file(s) for JSON conversion (instead of default filename).",
                         metavar="JSON_FILE")
     parser.add_argument('--version', '-V',
                         action='store_true',
@@ -41,32 +43,35 @@ def main() -> None:
         sys.exit(0)
 
     # check params
-    if not args.mtf_file:
+    if not args.mtf_file or (args.json_file and len(args.mtf_file) != len(args.json_file)):
+        if args.json_file and len(args.mtf_file) != len(args.json_file):
+            print("\nError: The number of JSON files must match the number of MTF files.")
         parser.print_help()
         sys.exit(1)
 
     # read MTF
-    path = Path(args.mtf_file)
-    if not path.exists():
-        print(f"File {path} does not exist!")
-        sys.exit(1)
-    try:
-        data = read_mtf(path)
-    except ConversionError as e:
-        print(f"Failed to convert '{path}': {e}")
-        sys.exit(1)
-
-    # convert to JSON and print or write to file
-    if args.convert:
-        json_path = Path(args.json_file) if args.json_file else path.with_suffix('.json')
-        try:
-            write_json(data, json_path)
-            print(f"Successfully saved JSON file '{json_path}'.")
-        except Exception as e:
-            print(f"Error: writing 'json_path' failed with '{e}'")
+    for i, mtf_file in enumerate(args.mtf_file):
+        path = Path(mtf_file)
+        if not path.exists():
+            print(f"File {path} does not exist!")
             sys.exit(1)
-    else:
-        print(json.dumps(data))
+        try:
+            data = read_mtf(path)
+        except ConversionError as e:
+            print(f"Failed to convert '{path}': {e}")
+            sys.exit(1)
+
+        # convert to JSON and print or write to file
+        if args.convert:
+            json_path = Path(args.json_file[i]) if args.json_file else path.with_suffix('.json')
+            try:
+                write_json(data, json_path)
+                print(f"Successfully saved JSON file '{json_path}'.")
+            except Exception as e:
+                print(f"Error: writing '{json_path}' failed with '{e}'")
+                sys.exit(1)
+        else:
+            print(json.dumps(data))
 
 
 if __name__ == "__main__":
