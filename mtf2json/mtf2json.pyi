6279a094ca6af7e40fe9bbeb6ac9c97f784db5e3
juk0de <git@kadidlo.de>
2024-07-11 06:56:51 +0200

    mypy: fixed mtf2json stub

--- mtf2json/mtf2json.pyi
+++ mtf2json/mtf2json.pyi
@@ -2,5 +2,13 @@ from pathlib import Path
 from typing import Dict, Any
 
 
+version: str
+mm_version: str
+
+
+class ConversionError(Exception):
+    ...
+
+
 def read_mtf(path: Path) -> Dict[str, Any]: ...
 def write_json(data: Dict[str, Any], path: Path) -> None: ...
