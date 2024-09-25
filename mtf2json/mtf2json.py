37288395a125448ee628a1fef945a9c2d94743aa
juk0de <5322305+juk0de@users.noreply.github.com>
2024-09-25 18:47:17 +0200

    version bump: 0.2.1

--- mtf2json/mtf2json.py
+++ mtf2json/mtf2json.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from typing import Dict, Any, Tuple, Union, Optional, List, cast, TextIO, Iterator
 
 
-version = "0.2.0"
+version = "0.2.1"
 mm_commit = "dfeb43e28132c2723ac8e3147e41b00960b989fd"
 
 
