ee6b61f3b96307b9dd86434566d9ad9f1ebb4de4
juk0de <5322305+juk0de@users.noreply.github.com>
2024-11-19 09:32:26 +0100

    version bump: 0.2.6

--- mtf2json/mtf2json.py
+++ mtf2json/mtf2json.py
@@ -35,7 +35,7 @@ from .heat_sinks import add_heat_sinks
 from .equipment import add_equipment_section
 
 
-version = "0.2.5"
+version = "0.2.6"
 mm_commit = "dfeb43e28132c2723ac8e3147e41b00960b989fd"
 
 
